d52e3d3e5c0005978edd5ece8d3c5958f1f2109a
Mark <mchellmer@gmail.com>
2023-04-12 18:48:35 +0100

    update dj to set content directly

--- data/files/botCode/dj.cs
+++ data/files/botCode/dj.cs
@@ -1,31 +1,46 @@
 using System;
+using System.IO;
 
 public class CPHInline
 {
-    string newDj;
-    string userInput = null;
+    string _newDj;
+    string _userInput;
+    string _djFilePath;
+
+    public void WriteToFile(string filePath, string value){
+        using (StreamWriter writer = new StreamWriter(filePath))
+        {
+            writer.WriteLine(value);
+        }
+    }
+
     public bool Execute()
     {
         foreach (var arg in args)
         {
             CPH.LogInfo($"LogVars :: {arg.Key} = {arg.Value}");
             if (arg.Key == "input0") {
-            	userInput = $"{arg.Value}";
-                CPH.LogInfo($"Set User to Input :: {userInput}");
-                CPH.LogInfo($"input is null or empty :: {String.IsNullOrEmpty(userInput)}");
+            	_userInput = $"{arg.Value}";
+                CPH.LogInfo($"Set User to Input :: {_userInput}");
+                CPH.LogInfo($"input is null or empty :: {String.IsNullOrEmpty(_userInput)}");
             }
         }
 
-		if(String.IsNullOrEmpty(userInput)){
+		if(String.IsNullOrEmpty(_userInput)){
                 CPH.LogInfo($"Default newDj");
-                newDj = "8-BitSaxBot";
-                CPH.AddUserToGroup(newDj, "DeeJay");
+                _newDj = "8-BitSaxBot";
 		}else{
-                newDj = userInput;
+                _newDj = _userInput;
         }
-        CPH.SetGlobalVar("newDj", newDj);
-        CPH.LogInfo($"Set newDJ to :: {newDj}");
+        CPH.AddUserToGroup(_newDj, "DeeJay");
+        CPH.LogInfo($"Set newDJ to :: {_newDj}");
+        WriteToFile(_djFilePath, _newDj);
 
         return true;
+    }
+
+    public CPHInline(){
+        _userInput = null;
+        _djFilePath = "C:/Users/mchel/OneDrive/Documents/0_Store/Twitch/Obs/text/deejay.txt";
     }
 }
